2d4d3be23f5223090e557befef3c2d413f37c64a
EC2 Default User <ec2-user@ip-172-31-69-176.ec2.internal>
2021-04-16 08:58:48 +0000

    devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,40 @@
 Rails.application.routes.draw do
+  devise_for :admins,controllers:{
+      sessions: "admins/sessions",
+      passwords: "admins/passwords",
+      registrations: "admins/registrations"
+  }
+  devise_for :customers, controllers:{
+      sessions: "public/sessions",
+      passwords: "public/passwords",
+      registrations: "public/registrations"
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: :public do
+      root :to => "homes#top"
+      get "/about" => "homes#about"
+      resources :items,only:[:index,:show]
+      resources :cart_items,only:[:index,:update,:create,:destroy]
+      delete "/cart_items/destroy_all" => "cart_items#destroy_all"
+      resource :customers,only:[:edit,:update,]
+      get "/customers/mypage" => "customers#show"
+      get "/customers/unsubscribe" => "customers#unsbscribe"
+      patch "/customers/withdraw" => "customers#withdraw"
+      resources :addresses,only:[:index,:edit,:create,:update,:destroy]
+      resources :orders,only:[:new,:create,:index,:show]
+      post "/orders/confirm" => "orders#confirm"
+      get "/orders/complete" => "orders#complete"
+  end
+ 
+  namespace :admin do
+      root :to => "homes#top"
+      resources :items,only:[:index,:show,:new,:create,:edit,:update]
+      resources :genres,only:[:index,:create,:edit,:update]
+      resources :customers,only:[:index,:show,:edit,:update]
+      resources :orders,only:[:show,:update] do
+      resources :order_details,only:[:update]
+      end
+  end
+
+
 end
