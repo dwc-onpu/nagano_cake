099d92cab0f4c29c405ba5415118c89761e4e96c
kouki9115 <kouki.n9115@gmail.com>
2021-04-18 08:29:23 +0000

    admin Finish

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,12 @@ class Item < ApplicationRecord
   attachment :image
   belongs_to :genre
 
-
+ #enum status: { 販売中: true, 販売停止中: false }
 
 end
+
+            #<%= if item.is_active == true
+               #puts "販売中"
+              #elsif item.is_active == false
+               #puts "販売停止中"
+              #end  %>
