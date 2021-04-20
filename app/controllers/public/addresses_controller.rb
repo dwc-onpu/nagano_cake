6a8aa52f31280e0c36529c08894af75484ee26d3
fumika88 <fuum14532@gmail.com>
2021-04-20 04:52:58 +0000

    address controller cart_item controller view 作成

---
+++ app/controllers/public/addresses_controller.rb
@@ -0,0 +1,46 @@
+class Public::AddressesController < ApplicationController
+
+  def create
+    address = Address.new(address_params)
+    address.customer_id = current_customer.id
+    if address.save
+    redirect_to addresses_path
+    else
+    address = Address.new(address_params)
+    address.customer_id = current_customer.id
+    render :index
+    end
+
+  end
+
+  def index
+    @address = Address.new
+    @addresses = current_customer.addresses
+
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+
+  end
+
+  def update
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to addresses_path
+
+  end
+
+  def destroy
+   address = Address.find(params[:id])
+   address.destroy
+   redirect_to addresses_path
+
+  end
+
+  private
+  # ストロングパラメータ
+  def address_params
+    params.require(:address).permit(:customer_id, :postal_code, :name, :address)
+  end
+end
---
+++ app/controllers/public/cart_items_controller.rb
@@ -0,0 +1,46 @@
+class Public::CartItemsController < ApplicationController
+
+    def index
+     @cart_items = current_customer.cart_items
+
+    end
+
+    def create
+     @cart_item = CartItem.new(cart_item_params)
+     @cart_item.customer_id = current_customer.id
+     @cart_item.item_id = params[:item_id]
+     @cart_item.save
+     redirect_to cart_items_path
+
+    end
+
+    def update
+        @cart_item = CartItem.find(params[:id])
+        @cart_item.update(cart_item_params)
+        redirect_to customers_cart_items_path
+    end
+
+
+    def destroy
+     @cart_item = CartItem.find(params[:id])
+     @cart_item.destroy
+     flash.now[:alert] = "#{@cart_item.item.name}を削除しました"
+     redirect_to cart_items_path
+    end
+
+    private
+
+    # カート商品を空ににする
+    def destroy_all
+     @cart_item = current_customer.cart_items
+     @cart_item.destroy_all
+     flash[:alert] = "カートの商品を全て削除しました"
+     redirect_to cart_items_path
+    end
+
+    def cart_item_params
+     params.require(:cart_item).permit(:amount, :item_id, :customer_id)
+    end
+
+
+end
