16a7aa9d15f32a585a0c3316f12f23cf89532360
EC2 Default User <ec2-user@ip-172-31-69-176.ec2.internal>
2021-04-22 04:50:54 +0000

    [fix]退会処理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
     
+    add_flash_types :success,:info,:warning,:danger
+    
     protected
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up,keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number])
